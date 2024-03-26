a688a850b2c8678a631ec08702bd5baedb70dae4
Thanh Tran <69281655+draytht@users.noreply.github.com>
2024-03-25 19:00:54 -0500

    Update manage_dances.php with more function with CRUD functionality

--- manage_dances.php
+++ manage_dances.php
@@ -1,6 +1,6 @@
-<?php include 'navigation.php';
+<?php 
 require_once('db_configuration.php'); 
-
+include 'navigation.php';
 // Create connection
 $conn = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_DATABASE);
 if ($conn->connect_error) {
@@ -25,35 +25,55 @@ $conn->close();
 <head>
     <meta charset="UTF-8">
     <title>Manage Dances</title>
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/2.0.0/css/dataTables.dataTables.min.css">
-    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
-    <script src="https://cdn.datatables.net/2.0.0/js/dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.flash.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.html5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.print.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css"> 
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.2.3/css/buttons.dataTables.min.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.flash.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.7.1/jszip.min.js"></script>   
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.68/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.68/vfs_fonts.js"></script>
 </head>
 <body>
-<button class="centered-button" onclick="window.location.href='create_dances.php';">Add New Dance</button>
+<div class="clearfix"></div>
+<div id="dataTableContainer">
+    <button class="centered-button" onclick="window.location.href='create_dances.php';">Add New Dance</button>
+</div>
 <style>
-  .centered-button {
-    background-color: #4CAF50; 
-    color: white; 
-    padding: 10px 20px; /
-    border: none; 
-    border-radius: 5px; 
-    margin-top: 20px; 
-    display: block; 
-    margin-left: auto; 
-    margin-right: auto; 
-    cursor: pointer; 
-    text-align: center; 
-  }
-  .centered-button:hover {
-    background-color: #45a049; 
-  }
-</style>
+    body {
+        margin-top: 0;
+    }
+    .navbar-default {
+        margin-bottom: 0;
+    }
+    .centered-button {
+        background-color: #4CAF50; 
+        color: white; 
+        padding: 10px 20px; 
+        border: none; 
+        border-radius: 5px; 
+        margin-top: 20px; 
+        display: block;   
+        margin-left: auto;  
+        margin-right: auto; 
+        cursor: pointer; 
+        text-align: center; 
+    }
+    .centered-button:hover {
+        background-color: #45a049; 
+    }
+    .clearfix::after {
+        content: "";
+        clear: both;
+        display: table;
+    }
+    #dataTableContainer {
+        padding-top: 450px; 
+    }
+    </style>
 <table id="dancesTable" class="display" style="width:100%">
     <thead>
         <tr>
@@ -89,38 +109,28 @@ $conn->close();
                 <td><?php echo htmlspecialchars($row['dance_key_words']); ?></td>
                 <td><?php echo htmlspecialchars($row['dance_status']); ?></td>
                 <td><?php echo htmlspecialchars($row['artist_images']); ?></td>
-                <td><button class="centered-button" onclick="editDance(<?php echo $row['dance_id']; ?>)">Modify</button></td>
+                <!-- <td><button class="centered-button" onclick="editDance(<?php echo $row['dance_id']; ?>)">Modify</button></td> -->
+                <td><a href="update.php?id=<?php echo $row['dance_id']; ?>" class="centered-button">Modify</a></td>
                 <td><button class="centered-button" onclick="deleteDance(<?php echo $row['dance_id']; ?>)">Delete</button></td>
-                <td><button class="centered-button" onclick="readDance(<?php echo $row['dance_id']; ?>)">Display</button></td>
+                <!-- <td><button class="centered-button" onclick="readDance(<?php echo $row['dance_id']; ?>)">Display</button></td> -->
+                <td><a href="search.php?id=<?php echo $row['dance_id']; ?>" class="centered-button">Display</a></td>
             </tr>
         <?php endforeach; ?>
     </tbody>
 </table>
 
 <script>
-    function createDance() {
-        alert('Create dance clicked!');
-    }
-
-    function editDance(id) {
-        alert('Edit dance id: ' + id + '.');
-    }
 
     function deleteDance(id) {
         if(confirm('Are you sure you want to delete this dance?')) {
         window.location.href = 'delete_dances.php?id=' + id;
-        alert('Dance id: ' + id + 'has been deleted!');
+        alert('Dance id: ' + id + ' has been deleted!');
     }
-    }
-    function readDance(id) {
-        // if(confirm('Are you sure you want to delete this dance?')) {
-        // window.location.href = 'read_dances.php?id=' + id;
-        // alert('Read dance id: ' + id + '.');
     }
 
     $(document).ready(function() {
         $('#dancesTable').DataTable({
-            dom: 'Bfrtip',
+            dom: 'lfrtipB',
             buttons: [
                 'copy', 'csv', 'excel', 'pdf', 'print'
             ],
